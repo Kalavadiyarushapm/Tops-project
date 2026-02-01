423dd689627616a036ad96be4df2c9464fc22edf
https://github.com/Kalavadiyarushapm/Assignment.git <kalavadiyarushap@gmail.com>
2026-02-01 11:26:42 +0530

    commited

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./css/header.css";
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Home";
-import About from "./About";
+// import About from "./About";
 import Contact from "./Contact";
 import Header from "./Header";
 import Footer from "./Footer";
